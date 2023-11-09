7cd9a4ce2e77d86eb0873b8428c2d318d4a729a7
Karan Kumar <78812510+KaranKumar-55@users.noreply.github.com>
2023-11-09 21:14:42 +0500

    Update app.py

--- app.py
+++ app.py
@@ -68,7 +68,8 @@ engine_size_options = [1.8, 3.2, 3.0, 2.8, 2.2, 6.7, 2.0, 2.5, 4.0, 5.4, 3.1, 3.
 engine_size_options.sort()
 selected_engine_size = st.selectbox("Select Engine Size (L)", engine_size_options)
 # Button to submit the selected features
-if st.button("Submit"):
+button = st.button("Submit")
+if button:
     # Display the selected features
     st.write(f"Selected Model Year: {selected_year}")
     st.write(f"Selected Vehicle Class: {selected_vehicle_class}")
@@ -76,6 +77,7 @@ if st.button("Submit"):
     st.write(f"Selected Transmission: {selected_transmission}")
     st.write(f"Selected Cylinders: {selected_cylinders}")
     st.write(f"Selected Engine Size (L): {selected_engine_size}")
+
 # Reset button to clear selections
 if st.button("Reset"):
     st.experimental_rerun()
@@ -90,121 +92,10 @@ Fuel_c_value = Fuel_c[0]
 user_input = [selected_year, encoded_vehicle_class,selected_engine_size, selected_cylinders,
               encoded_transmission , encoded_fuel_type, Fuel_c_value]
 # Use the loaded model to make predictions
-predictions = lr_model.predict([user_input])
-# Display the predictions
-st.write(f"CO2 Emissions : {predictions[0]:.2f} g/km")
+if not button:
+    st.write("Please click on Submit button to get the prediction")
+else:
+    predictions = lr_model.predict([user_input])
+    # Display the predictions
+    st.write(f"CO2 Emissions : {predictions[0]:.2f} g/km")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# # Dropdown for selecting Vehicle Class
-# vehicle_class_options = ['subcompact', 'compact', 'two-seater', 'station wagon', 'minicompact', 'mid-size', 'full-size', 'pickup truck', 'van', 'suv', 'other']
-# selected_vehicle_class = st.selectbox("Select Vehicle Class", vehicle_class_options)
-
-
-
-
-# # Dropdown for selecting Fuel Type
-# fuel_type_options = ['Regular gasoline', 'Premium gasoline', 'Diesel', 'Natural gas', 'Ethanol']
-# selected_fuel_type = st.selectbox("Select Fuel Type", fuel_type_options)
-
-
-# # Dropdown for selecting Transmission
-# transmission_options = ['Automatic', 'Manual', 'Automatic of Selective type', 'CVT', 'Automated Manual']
-# selected_transmission = st.selectbox("Select Transmission", transmission_options)
-
-
-# import streamlit as st
-
-# # Title of the app
-# st.title("Feature Selection")
-
-# # Dropdown for selecting Model Year with "Select" as the initial value
-# selected_year = st.selectbox("Select Model Year", ["Select"] + list(map(str, range(1995, 2024))))
-
-
-# # Mapping for Fuel Type
-# fuel_type_mapping = {
-#     "Select": None,  # You can set a default value like None for "Select"
-#     "Regular gasoline": 0,
-#     "Premium gasoline": 1,
-#     "Diesel": 2,
-#     "Natural gas": 3,
-#     "Ethanol": 4
-# }
-# selected_fuel_type = st.selectbox("Select Fuel Type", list(fuel_type_mapping.keys()))
-
-# # Get the encoded value based on user selection
-# encoded_fuel_type = fuel_type_mapping[selected_fuel_type]
-
-# # Mapping for Transmission
-# transmission_mapping = {
-#     "Select": None,  # You can set a default value like None for "Select"
-#     "Automatic": 0,
-#     "Manual": 1,
-#     "Automatic of Selective type": 2,
-#     "CVT": 3,
-#     "Automated Manual": 4
-# }
-# selected_transmission = st.selectbox("Select Transmission", list(transmission_mapping.keys()))
-
-# # Get the encoded value based on user selection
-# encoded_transmission = transmission_mapping[selected_transmission]
-
-# # Button to submit the selected features
-# if st.button("Submit"):
-#     # Display the selected features
-#     st.write(f"Selected Model Year: {selected_year}")
-#     st.write(f"Selected Vehicle Class: {encoded_vehicle_class}")
-#     st.write(f"Selected Fuel Type: {encoded_fuel_type}")
-#     st.write(f"Selected Transmission: {encoded_transmission}")
-   
-# # Reset button to clear selections
-# if st.button("Reset"):
-#     st.experimental_rerun()
